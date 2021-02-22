9bdce556d9fe6585d124be83cffb147837a7b1cb
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-02-22 20:49:18 +0100

    Update Switch_Loop.py

--- src/Switch_Loop.py
+++ src/Switch_Loop.py
@@ -26,7 +26,13 @@ print("3 - Multiplication")
 print("4 - Division")
 
 #Take input from user
-choice = input("What kind of operation should be performed? [Insert one of the options(1 2 3 4)]: ")
-x = input("Enter first number: ")
-y = input("Enter second number: ")
-print (switch(choice))
+while True:
+    choice = input("What kind of operation should be performed? [Insert one of the options(1 2 3 4)]: ")
+    if choice in ('1', '2', '3', '4'):
+        x = input("Enter first number: ")
+        y = input("Enter second number: ")
+    print (switch(choice))
+    break
+    
+    
+    
