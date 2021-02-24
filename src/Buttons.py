741a80b944cc2da3e24914d876fe676adcd284e5
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-02-24 23:34:45 +0100

    Update Buttons.py

--- src/Buttons.py
+++ src/Buttons.py
@@ -1,9 +1,9 @@
-# install the State Tool
+# Install the State Tool
 # Windows: 
-IEX(New-Object Net.WebClient).downloadString('https://platform.activestate.com/dl/cli/install.ps1')
-state activate --default ActiveState/ActivePython-3.8
+# IEX(New-Object Net.WebClient).downloadString('https://platform.activestate.com/dl/cli/install.ps1')
+# state activate --default ActiveState/ActivePython-3.8
 
-Linux, Mac : 
-sh <(curl -q https://platform.activestate.com/dl/cli/install.sh)
-state activate --default ActiveState/ActivePython-3.8 
+# Linux, Mac : 
+# sh <(curl -q https://platform.activestate.com/dl/cli/install.sh)
+# state activate --default ActiveState/ActivePython-3.8 
 
