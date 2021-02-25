b1a7e2e81348b3695c82c748186ff12c54f0fb55
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-02-25 23:24:01 +0100

    Update Buttons.py

--- src/Buttons.py
+++ src/Buttons.py
@@ -7,3 +7,20 @@
 # sh <(curl -q https://platform.activestate.com/dl/cli/install.sh)
 # state activate --default ActiveState/ActivePython-3.8 
 
+
+from tkinter import *
+
+root = Tk()
+root.title("PyCalculator")
+
+a = Entry(root, width=35, borderwidth=5)
+a.grid(row=0, column=0, cloumnspan=3, ipadx=10, pady=10)
+
+def button():
+    return
+
+button1 = Button(root, text="1", padx=40, pady=40, command=button)
+button2 = Button(root, text="2", padx=40, pady=40, command=button)
+
+button1.grid(row=3, column=0)
+button2.grid(row=3, column=1)
