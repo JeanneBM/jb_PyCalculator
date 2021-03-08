310c04be84877978634fa300e42bb1bed3e74a09
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-03-08 23:49:24 +0100

    Update unittest_pycalculator.py

--- test/unittest_pycalculator.py
+++ test/unittest_pycalculator.py
@@ -1,1 +1,25 @@
+import unittest
 
+from ClassyCalc import PyCalculator
+
+class unittest_pycalculator(unittest.TestCase):
+  
+  def setUp(self):
+    self.calculator = PyCalculator()
+
+  def test_add(self):
+    self.assertEqual(self.calculator.addition(4,7), 11)  
+    
+  def test_subtract(self):
+    self.assertEqual(self.calculator.subtraction(10,5), 5)  
+    
+  def test_multiply(self):
+    self.assertEqual(self.calculator.multiplication(3,7), 21) 
+    
+  def test_divide(self):
+    self.assertEqual(self.calculator.division(10,2), 5)
+    
+if __name__ == "__main__":
+  unittest.main()
+  
+  
