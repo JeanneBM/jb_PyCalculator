7695937ef8e1eca280dc96bfb09ea733c8e8ce77
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-03-11 23:31:37 +0100

    Update rest_api.py

--- src/rest api/rest_api.py
+++ src/rest api/rest_api.py
@@ -5,3 +5,35 @@ app = Flask(__name__)
 def main():
     return render_template('server.html')
 
+@app.route('/case', methods=['POST'])
+def case(oper_value=oper_value):
+    if request.method == 'POST':
+        x = request.form['x']
+        y = request.form['y']
+        choice = request.form['choice']
+
+        if choice == 'addition':
+            oper_value = x + y
+            return render_template('server.html', oper_value=oper_value)
+
+        elif choice == 'subtraction':
+            oper_value = x - y
+            return render_template('server.html', oper_value=oper_value)
+
+        elif choice == 'multiplication':
+            oper_value = x * y
+            return render_template('server.html', oper_value=oper_value)
+
+        elif choice == 'division':
+            sum = x / y
+            return render_template('server.html', oper_value=oper_value)
+        else:
+            return render_template('server.html')
+
+
+if __name__ == ' __main__':
+    app.debug = True
+    app.run()
+    
+    
+    
