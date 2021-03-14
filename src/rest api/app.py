13e207161af039a78aec99a6a3c39fbd39b8ba60
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-03-14 04:13:53 +0100

    Update and rename rest_api.py to app.py

--- src/rest api/app.py
+++ src/rest api/app.py
@@ -1,49 +1,39 @@
-'''
-Flask
-request
-render_templete
-'''
+from flask import Flask, render_template, request
 
-from flask import Flask, request, render_template
-
-# Application declaration
 app = Flask(__name__)
 
 
-# Start an application and declarate the main function
 @app.route('/')
 def main():
     return render_template('app.html')
 
 
-# Clarification of route
 @app.route('/send', methods=['POST'])
 def send(sum=sum):
     if request.method == 'POST':
         x = request.form['x']
         y = request.form['y']
-        choice = request.form['choice']
+        operation = request.form['operation']
 
-        if choice == 'addition':
-            oper_value = x + y
+        if operation == 'addition':
+            sum = float(x) + float(y)
             return render_template('app.html', sum=sum)
 
-        elif choice == 'subtraction':
-            oper_value = x - y
+        elif operation == 'subtraction':
+            sum = float(x) - float(y)
             return render_template('app.html', sum=sum)
 
-        elif choice == 'multiplication':
-            oper_value = x * y
+        elif operation == 'multiplication':
+            sum = float(x) * float(y)
             return render_template('app.html', sum=sum)
 
-        elif choice == 'division':
-            oper_value = x / y
+        elif operation == 'division':
+            sum = float(x) / float(y)
             return render_template('app.html', sum=sum)
-
         else:
             return render_template('app.html')
 
 
 if __name__ == ' __main__':
     app.debug = True
-    app.run() 
+    app.run()
