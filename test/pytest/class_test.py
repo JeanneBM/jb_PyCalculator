cbc4877a66e93cefd6adfc8486bde6c0a4b18d3f
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-03-21 23:11:14 +0100

    Update class_test.py

--- test/pytest/class_test.py
+++ test/pytest/class_test.py
@@ -1,60 +1,36 @@
 import pytest
 from src.classy_calc import PyCalculator
 
-# "Constants"
 x = 3.0
 y = 2.0
 
-
-# Fixtures
-
 @pytest.fixture
 def calculator():
-    return PyCalculator()
+    return PyCalculator(x,y)
 
-
-# Helpers
-
-def verify_answer(expected, answer, last_answer):
+def verify_answer(expected, answer):
     assert expected == answer
-    assert expected == last_answer
-
-
-# Test Cases
-
-def test_last_answer_init(calculator):
-    assert calculator.last_answer == 0.0
-
 
 def test_add(calculator):
-    answer = calculator.addition(x, y)
-    verify_answer(5.0, answer, calculator.last_answer)
+    answer = calculator.addition()
+    verify_answer(5.0, answer)
 
 
 def test_subtract(calculator):
-    answer = calculator.subtraction(x, y)
-    verify_answer(1.0, answer, calculator.last_answer)
-
-
-def test_subtract_negative(calculator):
-    answer = calculator.subtraction(x, y)
-    verify_answer(-1.0, answer, calculator.last_answer)
+    answer = calculator.subtraction()
+    verify_answer(1.0, answer)
 
 
 def test_multiply(calculator):
-    answer = calculator.multiplication(x, y)
-    verify_answer(6.0, answer, calculator.last_answer)
+    answer = calculator.multiplication()
+    verify_answer(6.0, answer)
 
 
-def test_divide(calculator):
-    answer = calculator.divide(x, y)
-    verify_answer(1.5, answer, calculator.last_answer)
+def test_division(calculator):
+    answer = calculator.divi()
+    verify_answer(1.5, answer)
 
 
-def test_divide_by_zero(calculator):
-    with pytest.raises(ZeroDivisionError) as e:
-        calculator.division(x, 0)
-    assert "Division by zero. We cannot perform this operation." in str(e.value)
 
 
     
