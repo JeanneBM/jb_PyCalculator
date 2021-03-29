74d63864b30bad4745d1d5f0416d73267b207920
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-03-29 03:27:38 +0200

    Update selenium_web_test.py

--- test/selenium_web_test.py
+++ test/selenium_web_test.py
@@ -13,7 +13,7 @@ def test_home_page():
     WHEN the '/' page is requested (GET)
     THEN check that the response is valid
     """
-    flask_app = create_app('flask_test.cfg')
+    flask_app = app('flask_test.cfg')
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
@@ -27,7 +27,7 @@ def test_home_page_post():
     WHEN the '/' page is is posted to (POST)
     THEN check that a '405' status code is returned
     """
-    flask_app = create_app('flask_test.cfg')
+    flask_app = app('flask_test.cfg')
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
