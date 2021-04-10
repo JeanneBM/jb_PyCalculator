462dd106ab6db95438df571229e6d3eb8809d489
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-10 23:32:53 +0200

    Update division_zero_test.py

--- tests/pytests/division_zero_test.py
+++ tests/pytests/division_zero_test.py
@@ -1,1 +1,16 @@
+import pytest
+from src.classy_calc import PyCalculator
 
+x = 3.0
+y = 0.0
+
+@pytest.fixture
+def calculator():
+    return PyCalculator(x,y)
+
+def verify_answer(expected, answer):
+    assert expected == answer
+
+def test_division(calculator):
+    answer = calculator.division()
+    verify_answer("Division by zero. We cannot perform this operation.", answer)
