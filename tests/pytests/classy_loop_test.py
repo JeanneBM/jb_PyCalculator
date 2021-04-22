d89d101112d4e902e944d8bef35b59d61a7f6d48
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-23 00:45:54 +0200

    Update classy_loop_test.py

--- tests/pytests/classy_loop_test.py
+++ tests/pytests/classy_loop_test.py
@@ -1,8 +1,6 @@
 import pytest
 from src.classy_calc import *
 
-choice = 1
-
 x = 3.0
 y = 2.0
 
@@ -17,9 +15,13 @@ def test_add(calculator):
     answer = calculator.addition()
     verify_answer(5.0, answer)
 
-    
+'''    
 @pytest.mark.parametrize("test_input, expected", [(1,5)])
 def choice_test(test_input, expected):
     assert choice(test_input) == expected
+''' 
+ 
+def test_main_choice_loop():
+    assert main(1) == print(calculator.addition())
     
     
