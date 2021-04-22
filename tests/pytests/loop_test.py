b6850eb024ff8df01c13d83fd8e2da83329805cb
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-22 21:41:42 +0200

    Update loop_test.py

--- tests/pytests/loop_test.py
+++ tests/pytests/loop_test.py
@@ -4,8 +4,7 @@ from src.classy_calc import *
 choice = 1
 
 x = 3.0
-y = 2.0
-'''
+
 @pytest.fixture
 def calculator():
     return PyCalculator(x,y)
@@ -17,17 +16,3 @@ def test_add(calculator):
     answer = calculator.addition()
     verify_answer(5.0, answer)
 
-
-def test_main():
-    assert choice(1) == test_add.answer
-
-'''
-
-
-def pytest_generate_tests(metafunc):
-    if "choice" in metafunc.fixturenames:
-        if metafunc.config.getoption("all"):
-            end = 5
-        else:
-            end = 2
-        metafunc.parametrize("choice", range(end))
