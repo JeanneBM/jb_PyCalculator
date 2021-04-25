545ea9d8005c4c0e742094b6c345a0107446c6bf
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-25 23:45:33 +0200

    Update api_test.py

--- tests/unittests/api_test.py
+++ tests/unittests/api_test.py
@@ -9,25 +9,11 @@ BASE_URL = 'http://localhost:5000/api/'
 
 class Test(unittest.TestCase):
     def create_app(self):
-        pass
-    '''
-        app = Flask(__name__)
-        self.app = app.test_client()
-        
-        app.config['DEBUG'] = True
-        app.config['TESTING'] = True
-        
-        self.app.testing = True 
-        return app
-    '''    
+
     def tear_down(self):
-        pass
-    
-    
+
     def web_test(self):
-        self.app.get('/')
-        self.assert_template_used('app.html')
-      
+
 if __name__ == ' __main__':
     unittest.main()
     
