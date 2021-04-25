9f3409ef9ba5e84ea452e553ee8f0dfd0c003b1a
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-26 00:46:20 +0200

    Update if_loop_test.py

--- tests/unittests/if_loop_test.py
+++ tests/unittests/if_loop_test.py
@@ -1,10 +1,29 @@
 import unittest
 from src.if_loop import *
 
+x = 3.0
+y = 2.0
 
-class TestCalculator:
 
-    def test_addition(self):
-        assert 40 == calculator.addition(20, 20)
+class class_unittest(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = PyCalculator(x,y)
+
+    def test_add(self):
+        self.assertEqual(self.calculator.addition(),5)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtraction(),1)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiplication(),6)
+
+    def test_division(self):
+        self.assertEqual(self.calculator.division(),1.5)
+
+
+if __name__ == "__main__":
+    unittest.main()
         
         
