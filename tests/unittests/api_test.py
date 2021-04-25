cf9983191ad511afa9e9572efd01e3ec7c5a5379
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-25 23:33:13 +0200

    Update api_test.py

--- tests/unittests/api_test.py
+++ tests/unittests/api_test.py
@@ -1,13 +1,13 @@
 import unittest
 from flask import Flask
 from flask import render_template
-from flask.ext.testing import LiveServerTestCase 
+
 
 from src.rest_api.app import *
 
 BASE_URL = 'http://localhost:5000/api/'
 
-class Test(LiveServerTestCase):
+class Test(unittest.TestCase):
     def create_app(self):
         app = Flask(__name__)
         app.config['DEBUG'] = True
@@ -17,9 +17,6 @@ class Test(LiveServerTestCase):
     def tear_down(self):
         pass
     
-    def flask_application_is_up_and_running_test(self):
-        response = urllib2.urlopen(self.get_server_url())
-        self.assertEqual(response.code, 200) 
     
     def web_test(self):
         self.app.get('/')
