fa53645ed34de1ff2e618b16f3756036ee61eac8
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-25 23:49:30 +0200

    Update api_test.py

--- tests/unittests/api_test.py
+++ tests/unittests/api_test.py
@@ -1,19 +1,27 @@
 import unittest
 from flask import Flask
 from flask import render_template
-
-
+from flask.ext.testing import TestCase
+   
 from src.rest_api.app import *
 
 BASE_URL = 'http://localhost:5000/api/'
 
+
 class Test(unittest.TestCase):
     def create_app(self):
-
+        return myapp
+    
+    def tear_down(self):
+        pass
+    
+    def web_test(self):
+        self.app.get('/')
+        self.assert_template_used('hello.html')    
+    
     def tear_down(self):
 
-    def web_test(self):
-
+      
 if __name__ == ' __main__':
     unittest.main()
     
