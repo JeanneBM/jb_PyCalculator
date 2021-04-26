a0d565dcd85c26f91f357e48d4dbe874e9787cf5
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-27 01:03:49 +0200

    Update if_loop_test.py

--- tests/unittests/if_loop_test.py
+++ tests/unittests/if_loop_test.py
@@ -2,15 +2,8 @@ import unittest
 from src.if_loop import *
 
 def test_add():
-  
-    x = 3.0
-    y = 2.0
+  assert addition((3,2)) == 5
 
- 
-    result = addition(x, y)
-  
-    assert result
-    assert addition(4,5)
-    
-test_add()
-print("Everything passed")
+if __name__ == "__main__":
+  test_add()
+  print("Everything passed")
