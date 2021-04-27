d172f11e915841df44f986a57c2e64c79218f9bc
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-27 23:24:29 +0200

    Update if_loop_test.py

--- tests/if_loop_test.py
+++ tests/if_loop_test.py
@@ -4,18 +4,6 @@ from test import support
 from src.rest_api import app
 
 class MyTest(unittest.TestCase):
-
-    def setUp(self):
-        self.app.config['TESTING'] = True
-        self.app.config['DEBUG'] = False
-        self.app = app.test_client()
-        
-    def create_app(self):
-        return app
-
-    def test_greeting(self):
-        self.app.get('/')
-        self.assert_template_used('app.html')
         
 if __name__ == '__main__':
     unittest.main()
